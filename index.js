41c89ec6ae42b88bb0f262fda6a97520b2255766
TDMsiro20 <dangphuong7x@gmail.com>
2023-08-19 19:35:02 +0700

    update

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
+// SIGN IN
 document.addEventListener("DOMContentLoaded", function () {
-    const loginForm = document.getElementById("login-form");
-  
-    loginForm?.addEventListener("submit", function (event) {
+  const registerForm = document.getElementById("register-form");
+
+  registerForm?.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm-password").value;
+    if (password !== confirmPassword) {
+      alert("Mật khẩu không khớp");
+      return;
+    }
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+    alert("Đã tạo thành công")
+    window.location.href="Loginform.html"
+  });
+});
+
+// LOG IN
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.getElementById("login-form");
+
+  loginForm?.addEventListener("submit", function (event) {
       event.preventDefault();
       const username = document.getElementById("username").value;
+      const password = document.getElementById("password").value;
       const registeredUsername = localStorage.getItem("username");
       const registeredPassword = localStorage.getItem("password");
       if (username !== registeredUsername || password !== registeredPassword) {
-        alert("Thông tin đăng nhập không chính xác");
-        return;
+          alert("Thông tin đăng nhập không chính xác");
+          return;
       }
       localStorage.setItem("isLogin", true);
-      alert("Đăng nhập thành công");
-    });
+      alert("Đăng nhập thành công")
+      window.location.href="Homepage.html"
   });
+});
+
+// LOG OUT
+const logoutButton = document.getElementById("logout-button");
+logoutButton?.addEventListener("click", function (event) {
+    event.preventDefault();
+    localStorage.removeItem("isLogin");
+    alert("Đăng xuất thành công");
+    window.location.reload();
+});
+
